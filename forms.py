6bf2923a1c80492ee69946dc71224eeea7707d2d
Lisandro Guerra <lisandro.digital@gmail.com>
2022-12-22 08:26:38 +0000

    create register form in form.py file

---
+++ forms.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import BooleanField, PasswordField, StringField, SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo, Length
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=3, max=25)])
+    email = StringField('Email', validators=[DataRequired(), Email(), Length(min=7, max=100)])
+    password = PasswordField('Password', validators=DataRequired())
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+

99bdaba9dea710c7aabe67f63a157aa54ddefd99
Lisandro Guerra <lisandro.digital@gmail.com>
2022-12-22 08:43:54 +0000

    correction on form.py file

--- forms.py
+++ forms.py
@@ -7,6 +7,7 @@ from wtforms.validators import DataRequired, Email, EqualTo, Length
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=3, max=25)])
     email = StringField('Email', validators=[DataRequired(), Email(), Length(min=7, max=100)])
-    password = PasswordField('Password', validators=DataRequired())
+    password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
 
